2f9be8eb45df455f719453e13a854bf72508653b
Sequence32 <dkbcraft1@gmail.com>
2016-12-29 20:01:52 -0500

    Added comments

--- brackets.cpp
+++ brackets.cpp
@@ -1,9 +1,6 @@
-#include <math.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <limits.h>
 #define MAXSIZE	 100000
 
 struct stack
@@ -24,12 +21,19 @@ const char *rightChar ="]})";
 
 int main(int argc, char* argv[])
 {
+	/*Start the stacks top at -1 we will add one every time until a left bracket 
+		is seen when we loop through the array of chars, when a right bracket is seen
+		if the right bracket matches the left bracket we pop of top off  the stack.
+		
+	*/
 	lastLeft.top=-1;
 	
+	//Numbering is the number of strings that the user will be prompt for
     int numString; 
     scanf("%d",&numString);
     for(int i = 0; i < numString; i++)
     {
+    	//This is just an array or pointer to memory location 
         char* s = (char *)malloc(10240 * sizeof(char));
         
         scanf("%s",s);
@@ -39,6 +43,7 @@ int main(int argc, char* argv[])
 		{
 			if((lastLeft.top == -1) &&(strchr(rightChar, *s)))
 			{
+				//I set this to 10 just to let the if outside the while know to print NO
 				lastLeft.top = 10;
 				break;
 			}
